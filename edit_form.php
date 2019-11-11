1501233f49c1a32e86dd2e7b6a6a53bb63fd75a1
Michael Vangelovski <michael.vangelovski@hotmail.com>
2019-11-12 09:37:46 +1100

    Update for CampusRoles profile field.

--- edit_form.php
+++ edit_form.php
@@ -108,9 +108,8 @@ class block_custom_site_links_edit_form extends block_edit_form {
         $type = 'text';
         $name = 'config_iconlinkcampusroles';
         $label = get_string('roles', 'block_custom_site_links');
-        $desc = get_string('roles_desc', 'block_custom_site_links');
         $options = array('size' => '50');
-        $repeatarray[] = &$mform->createElement($type, $name, $label, $desc, $options);
+        $repeatarray[] = &$mform->createElement($type, $name, $label, $options);
 
         $type = 'text';
         $name = 'config_iconlinkorder';
