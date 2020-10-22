82330da3529577ef0e7c03b953fef17773ab7326
Michael Vangelovski <michael.vangelovski@hotmail.com>
2020-10-22 15:49:55 +1100

    Mobile: do not render block if no links.

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -56,7 +56,10 @@ class mobile {
         $data = block_custom_site_links_init($blockinstanceid, array('mobile'));
 
         // Render the initial html.
-        $html = $OUTPUT->render_from_template('block_custom_site_links/mobile', $data);
+        $html = '';
+        if ($data['linktypes'] != '') {
+            $html = $OUTPUT->render_from_template('block_custom_site_links/mobile', $data);
+        }
         return [
             'templates' => [
                 [
