43e6e99d81945569fa74ab235b2d7ab645cd9ce7
Veronica Bermegui <verisb@gmail.com>
2021-01-07 09:02:04 +1100

    delete repeated roles

--- lang/en/block_custom_site_links.php
+++ lang/en/block_custom_site_links.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -38,8 +37,7 @@ $string['platforms'] = 'Platforms';
 $string['delete'] = 'Delete';
 $string['deletedesc'] = 'Delete this link';
 $string['roles'] = 'Roles';
-$string['roles_desc'] = '<div style="position: fixed;top: 68px;right: -1px;width: 270px;overflow-wrap: break-word;color: rgba(0,0,0,0.7);background-color: #f5f7f9;padding: 10px;border-radius: 2px;border: 1px solid rgba(0,0,0,0.1);z-index: 999;font-family: monospace;font-size: 12px;"> <span style="font-weight: bold;font-family: sans-serif;margin-right: 7px;">Roles</span>            <br>Early Learning Centre:Staff,<br>Early Learning Centre:Parents,<br>Early Learning Centre:Students,<br>Early Learning Centre:Future Parents,<br>Primary Red Hill:Staff,<br>Primary Red Hill:Parents,<br>Primary Red Hill:Students,<br>Primary Red Hill:Future Parents,<br>Northside:Staff,<br>Northside:Parents, <br>Northside:Students,<br>Northside:Future Parents,<br>Primary Red Hill:Staff,
-<br>Primary Red Hill:Parents,<br>Primary Red Hill:Students,<br>Primary Red Hill:Future Parents,<br>Senior School:Staff,<br>Senior School:Parents,<br>Senior School:Students,<br>Senior School:Future Parents,<br>Primary Red Hill:Admin Staff,<br>Whole School:Admin Staff<br>Wildcard -- *<br>Regex -- .*:staff</div>';
+$string['roles_desc'] = ' <div style="position: fixed;top: 68px;right: -1px;width: 270px;overflow-wrap: break-word;color: rgba(0,0,0,0.7);background-color: #f5f7f9;padding: 10px;border-radius: 2px;border: 1px solid rgba(0,0,0,0.1);z-index: 999;font-family: monospace;font-size: 12px;"> <span style="font-weight: bold;font-family: sans-serif;margin-right: 7px;">Roles</span> <br>Early Learning Centre:Staff,<br>Early Learning Centre:Parents, <br>Early Learning Centre:Students, <br>Early Learning Centre:Future Parents, <br>Primary Red Hill:Staff, <br>Primary Red Hill:Parents, <br>Primary Red Hill:Students, <br>Primary Red Hill:Future Parents, <br>Northside:Staff, <br>Northside:Parents, <br>Northside:Students, <br>Northside:Future Parents, <br>Senior School:Staff, <br>Senior School:Parents, <br>Senior School:Students, <br>Senior School:Future Parents, <br>Primary Red Hill:Admin Staff, <br>Whole School:Admin Staff <br>Wildcard -- * <br>Regex -- .*:staff </div>';
 $string['years'] = 'Year Level';
 $string['target'] = 'Target';
 $string['targetdesc'] = 'Open in new window';
@@ -50,7 +48,7 @@ $string['sorticonsalpha_desc'] = 'This option will cause icon links to be sorted
 $string['sorttextalpha'] = 'Sort alphabetically';
 $string['sorttextalpha_desc'] = 'This option will cause text links to be sorted by label in alphabetical order.';
 
-$string['validroles'] = 'Early Learning Centre:Staff,Early Learning Centre:Parents,Early Learning Centre:Students,Early Learning Centre:Future Parents,Primary Red Hill:Staff,Primary Red Hill:Parents,Primary Red Hill:Students,Primary Red Hill:Future Parents,Northside:Staff,Northside:Parents,Northside:Students,Northside:Future Parents,Primary Red Hill:Staff,Primary Red Hill:Parents,Primary Red Hill:Students,Primary Red Hill:Future Parents,Senior School:Staff,Senior School:Parents,Senior School:Students,Senior School:Future Parents,Primary Red Hill:Admin Staff,Whole School:Admin Staff,*,.*,Disabled';
+$string['validroles'] = 'Early Learning Centre:Staff,Early Learning Centre:Parents,Early Learning Centre:Students,Early Learning Centre:Future Parents,Primary Red Hill:Staff,Primary Red Hill:Parents,Primary Red Hill:Students,Primary Red Hill:Future Parents,Northside:Staff,Northside:Parents,Northside:Students,Northside:Future Parents,Senior School:Staff,Senior School:Parents,Senior School:Students,Senior School:Future Parents,Primary Red Hill:Admin Staff,Whole School:Admin Staff,*,.*,Disabled';
 
 $string['validyears'] = '1,2,3,4,5,6,7,8,9,10,11,12,*';
 $string['errorrole'] = 'Please provide valid role(s)';
